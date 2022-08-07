2257ce6f14a38d8aff0213f2aa595f45027453ad
Clupai8o0 <clupaio4@gmail.com>
2022-08-07 12:58:35 +0300

    basics js

--- js/stocks.js
+++ js/stocks.js
@@ -1,1 +1,13 @@
- 
+d3.json("assets/1y.json").then((data) => {
+	const dateParse = d3.timeParse("%Y-%m-%d");
+
+	data = data.map((d) => {
+		return { close: d.close, data: dateParse(d.date) };
+	});
+
+	const minDate = d3.min(data, (d) => d.date);
+	const maxDate = d3.max(data, (d) => d.date);
+
+	const minClose = d3.min(data, (d) => d.close);
+	const maxClose = d3.max(data, (d) => d.close);
+});
